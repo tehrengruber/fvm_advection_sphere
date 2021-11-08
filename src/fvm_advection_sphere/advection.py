Bugfixes & extensions - Velocity is now a (time-constant) field - Changed initial density profile - Fixed coordinate array scaling in atlas mesh - Export atlas vertex flags - Fixed bug in Atlas connectivity

src/fvm_advection_sphere/advection.py
@@ -14,7 +14,7 @@ def fvm_advect(
     for e in range(0, mesh.num_edges):
         # upwind flux (instructive)
         v1, v2 = mesh.e2v[e, :]
-        weighted_normal_velocity = np.dot(vel, mesh.dual_face_normal_weighted[e])  # velocity projected onto the normal
+        weighted_normal_velocity = np.dot(vel[e], mesh.dual_face_normal_weighted[e])  # velocity projected onto the normal
         if weighted_normal_velocity > 0:
             flux[e] = rho[v1] * weighted_normal_velocity
         else:
@@ -23,6 +23,6 @@ def fvm_advect(
     # compute density in the next timestep
     for v in range(0, mesh.num_vertices):
         #rho[v] = rho - δt / mesh.volume[v] * sum(flux[e] * face_orientation[v, local_e] for local_e, e in enumerate(mesh.v2e[v, :]))
-        rho[v] = rho[v] - δt * sum(flux[e] * mesh.dual_face_orientation[v, local_e] for local_e, e in enumerate(mesh.v2e[v, :]))
+        rho[v] = rho[v] - δt / mesh.vol[v] * sum(flux[e] * mesh.dual_face_orientation[v, local_e] for local_e, e in enumerate(mesh.v2e[v, :]))
 
 

src/fvm_advection_sphere/utils/atlas_mesh.py
@@ -55,7 +55,7 @@ def _atlas_connectivity_to_numpy(atlas_conn, *, out=None, skip_neighbor_indicato
         cols = atlas_conn.cols(i)
         for nb in range(cols):
             out[i, nb] = atlas_conn[i, nb]
-        out[i, cols+1:]=-1
+        out[i, cols:]=-1
 
     return out
 
@@ -72,8 +72,9 @@ def setup_mesh(grid=StructuredGrid("O32"), radius=6371.22e03, config=None):
     build_node_to_cell_connectivity(mesh)
     build_median_dual_mesh(mesh)
 
-    num_vertices = mesh.nodes.size
+    num_cells = mesh.cells.size
     num_edges = mesh.edges.size
+    num_vertices = mesh.nodes.size
 
     #
     # connectivities
@@ -85,17 +86,20 @@ def setup_mesh(grid=StructuredGrid("O32"), radius=6371.22e03, config=None):
     c2v = _atlas_connectivity_to_numpy(mesh.cells.node_connectivity)
     c2e = _atlas_connectivity_to_numpy(mesh.cells.edge_connectivity)
 
-    assert v2e.shape[0] == mesh.nodes.size
-    assert v2c.shape[0] == mesh.nodes.size
-    assert e2v.shape[0] == mesh.edges.size
-    assert e2c.shape[0] == mesh.edges.size
-    assert c2v.shape[0] == mesh.cells.size
-    assert c2e.shape[0] == mesh.cells.size
+    assert v2e.shape[0] == num_vertices
+    assert v2c.shape[0] == num_vertices
+    assert e2v.shape[0] == num_edges
+    assert e2c.shape[0] == num_edges
+    assert c2v.shape[0] == num_cells
+    assert c2e.shape[0] == num_cells
+
+    # flags
+    vertex_flags = np.array(mesh.nodes.flags(), copy=False)
 
     #
     # geometrical properties
     #
-    points = np.array(mesh.nodes.lonlat, copy=False)
+    points = np.array(mesh.nodes.lonlat, copy=False) * deg2rad * radius
 
     # face orientation
     edges_per_node = mesh.nodes.edge_connectivity.maxcols
@@ -130,6 +134,8 @@ def setup_mesh(grid=StructuredGrid("O32"), radius=6371.22e03, config=None):
         v2e=v2e,
         e2v=e2v,
         e2c=e2c,
+        # flags
+        vertex_flags=vertex_flags,
         # geometry
         points=points,
         vol=vol,

src/fvm_advection_sphere/utils/vis.py
@@ -41,10 +41,17 @@ def make_dataset_from_arrays(
 
     # For pyvista, the format of each primitive in the list should be:
     #     (n_items, item_0, item_1, ... item_n)
-    prepared_cells = np.concatenate(
+    def prepare_conn(conn):
+        num_neighbors = np.add.reduce(np.where(conn != -1, 1, 0), axis=1)
+        tmp = np.concatenate((num_neighbors[:, None], conn), axis=1)
+        return tmp[np.concatenate((np.full((conn.shape[0], 1), True, dtype=bool), conn != -1), axis=1)]
+
+    prepared_cells = prepare_conn(cells)
+    prepared_edges = prepare_conn(edges)
+    prepared_cells2 = np.concatenate(
         (np.asarray([cells.shape[-1]] * len(cells))[:, None], cells), axis=1
     )
-    prepared_edges = np.concatenate(
+    prepared_edges2 = np.concatenate(
         (np.asarray([edges.shape[-1]] * len(edges))[:, None], edges), axis=1
     )
 

tests/advection.py
@@ -7,37 +7,39 @@ from fvm_advection_sphere.advection import fvm_advect
 
 import fvm_advection_sphere.utils.vis as vis
 
-# parameters
-vel = np.array([1, 0]) # velocity
-δt = 1 # time step
-niter = 40
+from atlas4py import StructuredGrid
 
 # initialize mesh
 #Ni = 100
 #Nj = 100
 #mesh = regular_mesh.setup_mesh(Ni, Nj, (0, Ni), (0, Nj))
-mesh = atlas_mesh.setup_mesh()
+mesh = atlas_mesh.setup_mesh(StructuredGrid("O32"))
+
+# parameters
+vel = np.zeros((mesh.num_edges, 2))
+vel[:, 0] = 60000
+δt = 1 # time step
+niter = 100
 
 # initialize fields
 rho = np.zeros(mesh.num_vertices)
-rho[1:10] = 1
-#rho[100:110] = 1
-#rho[200:210] = 1
+origin = mesh.points.min(axis=0)
+extent = mesh.points.max(axis=0)-mesh.points.min(axis=0)
+xlim = (min(mesh.points[:, 0]), max(mesh.points[:, 0]))
+ylim = (min(mesh.points[:, 1]), max(mesh.points[:, 1]))
+for v in range(0, mesh.num_vertices):
+    rel_distance_from_origin = np.linalg.norm((mesh.points[v, :]-origin))/np.linalg.norm(extent)
+    if rel_distance_from_origin < 0.2:
+        rho[v] = 1
 
 vis.start_pyvista()
 
-#c2v = mesh.c2v[np.invert(mesh.cflags_periodic)]
 c2v = mesh.c2v
 ds = vis.make_dataset_from_arrays(mesh.points, edges=mesh.e2v, cells=c2v, vertex_fields={"rho": rho})
 
-#grid = ds["cells"]
-#grid.point_data["rho"] = ds["vertices"].point_data["rho"]
-#pl = pv.Plotter()
-#pl.add_mesh(grid, show_edges=True, line_width=2, interpolate_before_map=True)
-#pl.show(cpos="xy", interactive_update=True, auto_close=False)
-
 p = vis.plot_mesh(ds, interpolate_before_map=True)
-p.show(cpos="xy", interactive_update=True, auto_close=False)
+p.show(cpos="xy", interactive_update=True, auto_close=False) # non-blocking
+#p.show(cpos="xy") # blocking
 
 for i in range(niter):
     fvm_advect(mesh, rho, vel=vel, δt=δt)