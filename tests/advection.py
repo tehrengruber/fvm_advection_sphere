3e91d859cbc1ab346574994fae2643e2c9ef67dd
Till Ehrengruber <till.ehrengruber@cscs.ch>
2021-11-09 16:01:11 +0100

    Added serialization and deserialization of meshes to allow running without atlas4py

--- tests/advection.py
+++ tests/advection.py
@@ -1,18 +1,36 @@
 import numpy as np
+import pyvista as pv
 
-import fvm_advection_sphere.utils.regular_mesh as regular_mesh
-import fvm_advection_sphere.utils.atlas_mesh as atlas_mesh
+import fvm_advection_sphere.mesh.regular_mesh as regular_mesh
+import fvm_advection_sphere.mesh.atlas_mesh as atlas_mesh
 from fvm_advection_sphere.advection import fvm_advect
 
 import fvm_advection_sphere.utils.vis as vis
 
-from atlas4py import StructuredGrid
-
 # initialize mesh
-#Ni = 100
-#Nj = 100
-#mesh = regular_mesh.setup_mesh(Ni, Nj, (0, Ni), (0, Nj))
-mesh = atlas_mesh.setup_mesh(StructuredGrid("O32"))
+mesh_type = "serialized" # atlas, atlas_from_file
+# regular mesh
+if mesh_type == "regular":
+    Ni = 100
+    Nj = 100
+    mesh = regular_mesh.setup_mesh(Ni, Nj, (0, Ni), (0, Nj))
+elif mesh_type == "atlas":
+    # atlas mesh
+    from atlas4py import StructuredGrid
+    grid = StructuredGrid("O32")
+    mesh = atlas_mesh.setup_mesh(grid)
+
+    import bz2, pickle
+    file = f"../mesh_data/atlas_{grid.name}.dat.bz2"
+    with bz2.BZ2File(file, 'wb') as fd:
+        pickle.dump(mesh, fd)
+elif mesh_type == "serialized":
+    import bz2, pickle
+    file = "../mesh_data/atlas_O32.dat.bz2"
+    with bz2.BZ2File(file, 'rb') as fd:
+        mesh = pickle.load(fd)
+else:
+    raise ValueError()
 
 # parameters
 vel = np.zeros((mesh.num_edges, 2))
@@ -29,7 +47,7 @@ origin = mesh.xyarc.min(axis=0)
 extent = mesh.xyarc.max(axis=0)-mesh.xyarc.min(axis=0)
 xlim = (min(mesh.xyarc[:,0]), max(mesh.xyarc[:,0]))
 ylim = (min(mesh.xyarc[:,1]), max(mesh.xyarc[:,1]))
-rsina = np.sin(mesh.xyrad[:,1]) 
+rsina = np.sin(mesh.xyrad[:,1])
 rcosa = np.cos(mesh.xyrad[:,1])
 g11[:] = 1.0 / rcosa[:]
 g22[:] = 1.0
